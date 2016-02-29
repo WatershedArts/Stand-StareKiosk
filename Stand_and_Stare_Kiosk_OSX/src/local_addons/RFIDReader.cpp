e37e3e2ee4a3e06e1a02ea6c110fce2c33b837c5
DHaylock <david.haylock@watershed.co.uk>
2016-02-29 13:19:03 +0000

    - Updated: RFID is now Threaded

--- Stand_and_Stare_Kiosk_OSX/src/local_addons/RFIDReader.cpp
+++ Stand_and_Stare_Kiosk_OSX/src/local_addons/RFIDReader.cpp
@@ -10,6 +10,7 @@
 //--------------------------------------------------------------
 void RFIDReader::setup(string deviceName,int removeDelay)
 {
+    // Connect Serial
     serial.setup(deviceName, 9600);
     removeTimeout = removeDelay;
     lastTagChangeTime = 0;
@@ -29,52 +30,88 @@ bool RFIDReader::isConnected()
     return serial.isInitialized();
 }
 //--------------------------------------------------------------
-void RFIDReader::update()
+void RFIDReader::start()
+{
+    startThread();
+}
+//--------------------------------------------------------------
+void RFIDReader::stop()
+{
+    stopThread();
+}
+//--------------------------------------------------------------
+void RFIDReader::threadedFunction()
+{
+    // Check if the Thread is running
+    while (isThreadRunning()) {
+        // Is the resource locked
+        if (lock()) {
+            // Is the Device Connected
+            if (isConnected()) {
+                while (serial.available() > 0) {
+                    
+                    // Get the Incoming Byte
+                    incomingByte = serial.readByte();
+                    
+                    if (incomingByte > 13) {
+                        tagBuffer[tagBufferIndex] = incomingByte;
+                        tagBufferIndex++;
+                    }
+                    // Fire the Tag up into the main app
+                    if (incomingByte == 3) {
+                        tagString = tagBuffer;
+                        ofNotifyEvent(newTag, tagString, this);
+                        
+                        // Clear the Tag
+                        for (int i = 0; i < tagBufferIndex; i++) {
+                            tagBuffer[i] = 0;
+                        }
+                        tagBufferIndex = 0;
+                    }
+                }
+                
+                tagPreviouslyPresent = tagPresent;
+                
+                // This is the Removal Script
+                // Need to Implement a Arduino based Version
+                string state = "0";
+                
+                if(state == "1") {
+                    tagPresent = true;
+                }
+                else {
+                    tagPresent = false;
+                }
+                
+                if(tagPresent != tagPreviouslyPresent) {
+                    lastTagChangeTime = ofGetElapsedTimeMillis();;
+                    sendRemove = !tagPresent;
+                }
+                
+                if(sendRemove == true && ((ofGetElapsedTimeMillis() - lastTagChangeTime) > removeTimeout)) {
+                    string ev = "Tag Removed";
+                    ofNotifyEvent(tagRemoved, ev, this);
+                    sendRemove = false;
+                }
+            }
+            unlock();
+            sleep(1);
+        }
+    }
+}
+//--------------------------------------------------------------
+string RFIDReader::getCurrentTag()
 {
     if (isConnected()) {
-        while (serial.available() > 0) {
-
-            incomingByte = serial.readByte();
-
-            if (incomingByte > 13) {
-                tagBuffer[tagBufferIndex] = incomingByte;
-                tagBufferIndex++;
-            }
-
-            if (incomingByte == 3) {
-                // Fire the Tag up into the main app
-                tagString = tagBuffer;
-                ofMessage msg("New Tag: " + ofToString(tagString));
-                ofSendMessage(msg);
-                
-                for (int i = 0; i < tagBufferIndex; i++) {
-                    tagBuffer[i] = 0;
-                }
-                tagBufferIndex = 0;
-            }
-        }
-    
-        tagPreviouslyPresent = tagPresent;
-
-        string state = "0";
-
-        if(state == "1") {
-            tagPresent = true;
+        if (tagString.length() > 0) {
+            return tagString;
         }
         else {
-            tagPresent = false;
+            return "No Tag";
         }
-
-        if(tagPresent != tagPreviouslyPresent) {
-            lastTagChangeTime = ofGetElapsedTimeMillis();;
-            sendRemove = !tagPresent;
-        }
-
-        if(sendRemove == true && ((ofGetElapsedTimeMillis() - lastTagChangeTime) > removeTimeout)) {
-            ofMessage msg("Card Removed");
-            ofSendMessage(msg);
-            sendRemove = false;
-        }
+    }
+    else {
+        return "No RFID Reader Attached";
     }
 }
 //--------------------------------------------------------------
@@ -83,6 +120,9 @@ string RFIDReader::getDebugString()
     stringstream datastream;
     string connectionStatus = (isConnected())? "Connected" : "Not Connected";
     string tagP = tagPresent ? "True" : "False";
+    datastream << "|----------------------------------" << endl;
+    datastream << "| RFID Reader" << endl;
+    datastream << "|----------------------------------" << endl;
     datastream << "| Current Tag: " << tagString << endl;
     datastream << "| New Tag " << tagP << endl;
     return datastream.str();
--- Stand_and_Stare_Kiosk_OSX/src/local_addons/RFIDReader.hpp
+++ Stand_and_Stare_Kiosk_OSX/src/local_addons/RFIDReader.hpp
@@ -12,28 +12,53 @@
 #include <stdio.h>
 #include "ofMain.h"
 
-class RFIDReader {
+class RFIDReader : public ofThread {
 
 public:
+    //! Setup the Serial Connection
     void setup(string deviceName,int removeDelay = 2000);
-    void update();
+    
+    //! Check if the Serial Device is Connected
     bool isConnected();
-    ofSerial serial;
+    
+    //! Simulate a new tag being added
+    void simulateNewTag(int which);
+    
+    //! Simulate Removal
+    void simulateTagRemoval();
+    
+    //! Start Threading
+    void start();
+    
+    //! Stop Threading
+    void stop();
+    
+    //! Get Debug Information
+    string getDebugString();
 
+    //! Get the Current Tag
+    string getCurrentTag();
+    
+    // Serial Object
+    ofSerial serial;
+    
+    // Events
+    ofEvent<string> newTag;
+    ofEvent<string> tagRemoved;
+
+private:
     char incomingByte;
     char tagBuffer[16];
     int tagBufferIndex;
-    
     bool tagPresent;
     bool tagPreviouslyPresent;
     bool sendRemove;
     long lastTagChangeTime;
     long removeTimeout;
     string tagString;
-    string getDebugString();
-
+    
 protected:
-
+    void threadedFunction();
 };
 
 #endif /* RFIDReader_hpp */
